e2fe3c79b7073f8b542eed621556da12840f5486
suparansharma <suparansharmap@gmail.com>
2023-03-18 17:39:41 +0600

    arcitecture done

---
+++ src/Dashboard.js
@@ -0,0 +1,103 @@
+import React, { useState } from 'react';
+import './Dashboard.css';
+import Header from './Components/Header';
+import Leftsidebar from './Components/Leftsidebar';
+import Footer from './Components/Footer';
+import DefaultPages from './Pages/DefaultPages/DefaultPages';
+import App from './App';
+
+function Dashboard() {
+    const [style, setStyle] = useState("navbar-nav bg-gradient-primary sidebar sidebar-dark accordion");
+
+    const changeStyle = () => {
+        if (style == "navbar-nav bg-gradient-primary sidebar sidebar-dark accordion")
+        {
+            setStyle("navbar-nav bg-gradient-primary sidebar sidebar-dark accordion toggled");
+        }
+        else{
+            setStyle("navbar-nav bg-gradient-primary sidebar sidebar-dark accordion")
+        }
+    };
+    const changeStyle1 = () => {
+        if (style == "navbar-nav bg-gradient-primary sidebar sidebar-dark accordion")
+        {
+            setStyle("navbar-nav bg-gradient-primary sidebar sidebar-dark accordion toggled1");
+        }
+        else{
+            setStyle("navbar-nav bg-gradient-primary sidebar sidebar-dark accordion")
+        }
+    };
+
+    return (
+        <div>
+            <body id="page-top">
+
+                {/*  <!-- Page Wrapper --> */}
+                <div id="wrapper">
+
+                    {/*  <!-- Sidebar --> */}
+                    <Leftsidebar/>
+                    {/*  <!-- End of Sidebar --> */}
+
+                    {/*  <!-- Content Wrapper --> */}
+                    <div id="content-wrapper" className="d-flex flex-column">
+
+                        {/*  <!-- Main Content --> */}
+                        <div id="content">
+
+                            {/*  <!-- Topbar --> */}
+                           <Header/>
+                            {/*  <!-- End of Topbar --> */}
+
+                            {/* <!-- Begin Page Content --> */}
+                            <div className="container-fluid">
+
+                                <App/>
+
+                            </div>
+                            {/*   <!-- /.container-fluid --> */}
+
+                        </div>
+                        {/*   <!-- End of Main Content -->
+
+                                        <!-- Footer --> */}
+                       <Footer/>
+                        {/* <!-- End of Footer --> */}
+
+                    </div>
+                    {/*  <!-- End of Content Wrapper --> */}
+
+                </div>
+                {/*  <!-- End of Page Wrapper -->
+
+                                <!-- Scroll to Top Button--> */}
+                <a className="scroll-to-top rounded" href="#page-top">
+                    <i className="fas fa-angle-up"></i>
+                </a>
+
+                {/*  <!-- Logout Modal--> */}
+                <div className="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+                    aria-hidden="true">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                                <button className="close" type="button" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">×</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                            <div className="modal-footer">
+                                <button className="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                                <a className="btn btn-primary" href="login.html">Logout</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </body>
+        </div>
+    )
+}
+
+export default Dashboard;
---
+++ src/Pages/Error/Error.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <>
+    <h1>404 this pages is not found</h1>
+    </>
+  )
+}
+
+export default Error
